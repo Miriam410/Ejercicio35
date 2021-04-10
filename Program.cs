3771f5fc06c80715f4c210f94b2cc5ca1bd3ee1b
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-10 03:53:20 -0300

    Ejercicio 35 y 36

--- Program.cs
+++ Program.cs
@@ -12,6 +12,16 @@ namespace Ejercicio35
         {
             /* 35. Solicitar el ingreso de un número al usuario, repitiendo la operación
             (presentado un mensaje de error hasta que el ingreso sea válido); */
+            ValidarIngresoNumero();
+            Console.WriteLine("--------------------------------");
+            /* 36. Solicitar el ingreso de una fecha al usuario, repitiendo la operación
+           (presentado un mensaje de error hasta que el ingreso sea válido); */
+            ValidarIngresoFecha();
+            Console.ReadKey();
+        }
+
+        private static void ValidarIngresoNumero()
+        {
             string numero;
             double salidaNum;
             bool flag = false;
@@ -23,13 +33,36 @@ namespace Ejercicio35
                 {
                     Console.WriteLine("Por favor, ingrese un valor numérico");
                 }
-                else 
+                else
                 {
                     flag = true;
                     Console.WriteLine($"Usted ha ingresado correctamente un número, que es {salidaNum}");
                     Console.WriteLine("Presione una tecla para salir");
                 }
-            } while (flag==false);
+            } while (flag == false);
+            Console.ReadKey();
+        }
+
+        private static void ValidarIngresoFecha()
+        {
+            string fecha;
+            DateTime salidaFecha;
+            bool flag = false;
+            do
+            {
+                Console.WriteLine("Ingrese una fecha");
+                fecha = Console.ReadLine();
+                if (!DateTime.TryParse(fecha, out salidaFecha))
+                {
+                    Console.WriteLine("Usted no ha ingresado una fecha");
+                }
+                else
+                {
+                    flag = true;
+                    Console.WriteLine($"La fecha ingresada es {salidaFecha}");
+                    Console.WriteLine("Presione una tecla para salir");
+                }
+            } while (flag == false) ;
             Console.ReadKey();
         }
     }

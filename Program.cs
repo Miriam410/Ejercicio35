c683d1378271874207282f7ead0772d9a87ac5c2
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-10 03:40:04 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 35. Solicitar el ingreso de un número al usuario, repitiendo la operación
+            (presentado un mensaje de error hasta que el ingreso sea válido); */
+            string numero;
+            double salidaNum;
+            bool flag = false;
+            do
+            {
+                Console.WriteLine("Ingrese un número");
+                numero = Console.ReadLine();
+                if (!double.TryParse(numero, out salidaNum))
+                {
+                    Console.WriteLine("Por favor, ingrese un valor numérico");
+                }
+                else 
+                {
+                    flag = true;
+                    Console.WriteLine($"Usted ha ingresado correctamente un número, que es {salidaNum}");
+                    Console.WriteLine("Presione una tecla para salir");
+                }
+            } while (flag==false);
+            Console.ReadKey();
+        }
+    }
+}
